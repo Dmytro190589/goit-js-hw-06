0578ef7ef8827a9ca54ea795feec305506b2bdbf
Dmytro Kharchenko <dmytro190589@gmail.com>
2022-10-24 13:17:26 +0300

    2 task

--- js/task-02.js
+++ js/task-02.js
@@ -6,4 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const listEl = document.querySelector('#ingredients');
 
+const newEl = ingredients.map(element => {
+  const oneEl = document.createElement('li');
+  oneEl.textContent = element;
+  oneEl.classList.add('item')
+  console.log(oneEl);
+  return oneEl;
+});
+
+listEl.append(...newEl);
+console.log(listEl);
