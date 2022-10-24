2e1231ad718db3e53beeb9ccde38c6f03a222586
Dmytro Kharchenko <dmytro190589@gmail.com>
2022-10-24 08:44:08 +0300

    1 task completed

---
+++ js/task-01.js
@@ -0,0 +1,32 @@
+'use script'
+
+const numberEl = document.querySelector('ul').children.length;
+console.log(`Number of categories : ${numberEl}`);
+
+const categorEl = document.querySelectorAll('h2');
+for (const cat of categorEl){
+    console.log('Category: ' + cat.textContent);
+    const sumEl = cat.nextElementSibling.children.length;
+    console.log(`Elements : ${sumEl}`);
+}
+
+// У нас легких шляхів не буває))) Так що так))
+
+
+
+
+
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
+// В результате, в консоли будут выведены такие сообщения.
+
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
