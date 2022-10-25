5a02adc31cdd8d1177a1e865b85b066d93501aea
Dmytro Kharchenko <dmytro190589@gmail.com>
2022-10-25 08:15:17 +0300

    task 4

---
+++ js/task-04.js
@@ -0,0 +1,31 @@
+
+'use script'
+
+const buttons = document.querySelector('#counter');
+const increment  = buttons.lastElementChild;
+const decrement   = buttons.firstElementChild;
+
+const spanButton = document.querySelector('#value');
+
+let counterValue = 0;
+
+decrement.addEventListener('click', ()=>{
+    counterValue -=1;
+    spanButton.textContent = counterValue;
+})
+increment.addEventListener('click', ()=>{
+    counterValue += 1;
+    spanButton.textContent = counterValue;
+})
+
+// Счетчик состоит из спана и кнопок, которые, при клике, должны увеличивать и уменьшать его значение на единицу.
+
+// <div id="counter">
+//   <button type="button" data-action="decrement">-1</button>
+//   <span id="value">0</span>
+//   <button type="button" data-action="increment">+1</button>
+// </div>
+
+// Создай переменную counterValue в которой будет храниться текущее значение счетчика и инициализируй её значением 0.
+// Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение счтетчика.
+// Обновляй интерфейс новым значением переменной counterValue.
